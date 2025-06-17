c99c33832738a9279921c087d77a9d9d419c23c0
Lynne.Lee <61216031+Churng@users.noreply.github.com>
2025-06-17 14:16:18 +0800

    修改

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,5 @@ const { defineConfig } = require("@vue/cli-service");
 
 module.exports = defineConfig({
 	transpileDependencies: true,
-	publicPath: process.env.NODE_ENV === "production" ? "/2025aromagotcha/" : "/",
+	publicPath: process.env.NODE_ENV === "production" ? "/2025Aromagatcha/" : "/",
 });
