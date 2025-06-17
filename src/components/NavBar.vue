d4f12f79d23069f4f2287f670cfb92b95d03d4c5
Lynne.Lee <61216031+Churng@users.noreply.github.com>
2025-06-17 14:24:12 +0800

    修改：nav 按鈕距離

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,24 +3,27 @@
     class="navbar p-3 mb-3"
     style="background-color: #e3f2fd;"
   >
-    <router-link to="/learn">
-      <button
-        type="button"
-        class="btn btn-warning"
-      >
-        學習本
-      </button>
-    </router-link>
-    <router-link to="/exam">
-      <button
-        type="button"
-        class="btn btn-outline-secondary"
-      >
-        考試去
-      </button>
-    </router-link>
+    <div class="d-flex gap-2">
+      <router-link to="/learn">
+        <button
+          type="button"
+          class="btn btn-warning"
+        >
+          學習本
+        </button>
+      </router-link>
+      <router-link to="/exam">
+        <button
+          type="button"
+          class="btn btn-outline-secondary"
+        >
+          考試去
+        </button>
+      </router-link>
+    </div>
   </nav>
 </template>
+
 
 <script>
 export default {
